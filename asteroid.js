30c17acdfaa447c9b4f602154aebe748860edd68
N-A-ML <up741736@myport.ac.uk>
2021-01-22 20:05:51 +0000

    22012021

--- asteroid.js
+++ asteroid.js
@@ -2,32 +2,24 @@
 
 // set up the scene, camera and renderer
 var scene = new THREE.Scene();
+
 var camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   1,
   1000
 )
-
 camera.position.z = 10;
 camera.position.x = 0;
 camera.position.y = 0;
-
-
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#e5e5e5");
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-
-
 // var requestID;
 document.body.appendChild(renderer.domElement)
-
-
-
-
 
 // make it responsive
 window.addEventListener('resize', () => {
@@ -65,30 +57,21 @@ mesh.position.set(4, 3, 3)
 mesh.rotation.set(0, 0, 0) //rotate object on x, y, z axes
 mesh.scale.set(1, 1, 1) //scale the object in the x, y, z dimensions
 
-// scene.add(mesh)
-// scene.updateMatrixWorld(true);
-
 // create for loop within async function to generate asteroids over time  https://stackoverflow.com/questions/60555457/javascript-three-js-how-to-pause-after-adding-an-object-to-three-js-scene
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-
-
 async function makemeshes() {
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < Infinity; i++) {
+   
     mesh = new THREE.Mesh(geometry.clone(), material);
-
-
+    
     // randomly position on x axis
     mesh.position.x = 2 + 5 * Math.random() - 5 * Math.random()
-
-
     mesh.position.y = 3
     mesh.position.z = 4
 
     await wait(1000);
-
-
-
+    
 
     //only make asteroids when the user is on the page
     if (document.visibilityState != 'visible') {
@@ -98,18 +81,7 @@ async function makemeshes() {
       scene.add(mesh)
     }
 
-
-
-
-
     console.log(i)
-
-
-
-
-
-
-
 
   }
 }
@@ -132,18 +104,6 @@ var render = function () {
     }
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
   scene.traverse(function (mesh) {
 
     if (mesh instanceof THREE.Mesh) {
@@ -158,18 +118,8 @@ var render = function () {
 
   });
 
-
-
   renderer.render(scene, camera);
-
-
 }
-
-
-
-
-
-
 
 requestAnimationFrame(render);
 render();
