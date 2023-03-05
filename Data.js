4cce18d1e16225e9557f68303355c37e177c73ec
N-A-ML <up741736@myport.ac.uk>
2023-03-05 16:26:35 +0000

    05032023

---
+++ Data.js
@@ -0,0 +1,25 @@
+//function showHide(target) {
+//    var targetElement = document.getElementById(target);
+//    targetElement.classList.toggle("hidden");
+//}
+
+
+function myFunction() {
+    var x = document.getElementById("JDA1PDF");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
+function myFunction2() {
+    var x = document.getElementById("JDA2PDF");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
